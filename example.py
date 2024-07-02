200dc0f0043d21b8f66a9ed67827fa44f28be820
Lukas Schäfer <lschaefer@dhcp-90-246.inf.ed.ac.uk>
2024-07-02 18:50:01 +0100

    WIP: gymnasium integration for smaclite

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import time
 
-import gym
+import gymnasium as gym
 import numpy as np
 
 import smaclite  # noqa
@@ -11,13 +11,12 @@ USE_CPP_RVO2 = False
 
 def main():
     env = "MMM2"
-    env = gym.make(f"smaclite/{env}-v0",
-                   use_cpp_rvo2=USE_CPP_RVO2)
+    env = gym.make(f"smaclite/{env}-v0", use_cpp_rvo2=USE_CPP_RVO2)
     episode_num = 20
     total_time = 0
     total_timesteps = 0
     for i in range(episode_num):
-        obs, info = env.reset(return_info=True)
+        obs, _ = env.reset()
         if RENDER:
             env.render()
         done = False
@@ -27,21 +26,20 @@ def main():
         timestep_no = 0
         while not done and timestep_no < 200:
             actions = []
-            avail_actions = info['avail_actions']
-            for info in range(env.n_agents):
-                avail_indices = [i for i, x
-                                 in enumerate(avail_actions[info])
-                                 if x]
+            avail_actions = env.unwrapped.get_avail_actions()
+            for i in range(env.unwrapped.n_agents):
+                avail_indices = [a for a, valid in enumerate(avail_actions[i]) if valid]
                 actions.append(int(np.random.choice(avail_indices)))
                 # time.sleep(1/2)
             timer = time.time()
-            obs, reward, done, info = env.step(actions)
+            obs, reward, done, truncated, info = env.step(actions)
             episode_time += time.time() - timer
             episode_reward += reward
             timestep_no += 1
         print(f"Total reward in episode {episode_reward}")
-        print(f"Episode {i} took {episode_time} seconds "
-              f"and {timestep_no} timesteps.")
+        print(
+            f"Episode {i} took {episode_time} seconds " f"and {timestep_no} timesteps."
+        )
         print(f"Average per timestep: {episode_time/timestep_no}")
         total_time += episode_time
         total_timesteps += timestep_no
